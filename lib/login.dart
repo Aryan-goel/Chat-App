d9f0e6b51451eddbc20541c296e9c21b32159653
91639 <goel.aryan2406@gmail.com>
2021-05-30 19:21:24 +0530

    login screen completed

--- lib/login.dart
+++ lib/login.dart
@@ -12,69 +12,95 @@ class _LoginState extends State<Login> {
     return Scaffold(
       appBar: PreferredSize(
           preferredSize: const Size.fromHeight(60), child: appBarMain(context)),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-          children: [
-            TextField(
-              style: inputFieldtextStyle(),
-              decoration: textFieldInputDecoration("Email"),
-            ),
-            TextField(
-                style: inputFieldtextStyle(),
-                decoration: textFieldInputDecoration("Password")),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                child: Text(
-                  "Forgot password?",
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 10,
+          alignment: Alignment.center,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
                   style: inputFieldtextStyle(),
+                  decoration: textFieldInputDecoration("Email"),
                 ),
-              ),
+                TextField(
+                    style: inputFieldtextStyle(),
+                    decoration: textFieldInputDecoration("Password")),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                    child: Text(
+                      "Forgot password?",
+                      style: inputFieldtextStyle(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.blue,
+                        Colors.blue,
+                      ]),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(height: 18),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.white,
+                        Colors.white,
+                      ]),
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Text(
+                    "Sign In with Google",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?  ",
+                      style: inputFieldtextStyle(),
+                    ),
+                    Text(
+                      "Register now !",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          decoration: TextDecoration.underline),
+                    )
+                  ],
+                )
+              ],
             ),
-            SizedBox(height: 8),
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.symmetric(vertical: 16),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Colors.blue,
-                    Colors.white,
-                  ]),
-                  borderRadius: BorderRadius.circular(20)),
-              child: Text(
-                "Sign In",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 8),
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.symmetric(vertical: 16),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Colors.blue,
-                    Colors.white,
-                  ]),
-                  borderRadius: BorderRadius.circular(20)),
-              child: Text(
-                "Sign In with Google",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
